496bf9e8606748b69463ccb739072ea83908de3e
Makeea <speedynyc@gmail.com>
2019-07-22 00:41:08 -0400

    Create MathCouter1-100.cs
    show numbers 1-100

---
+++ MathCouter1-100.cs
@@ -0,0 +1,20 @@
+using System;  
+public class MathCouter1-100  
+{  
+    public static void Main()  
+    {
+    int i;
+    Console.Write("\n\n");
+    Console.Write("Display numbers:\n");
+    Console.Write("---------------------------------------");
+    Console.Write("\n\n");
+	
+     Console.WriteLine("1 to 100 numbers:");
+
+	for (i=1;i<=100;i++)
+	{      
+     Console.Write("{0} ",i);
+	}
+    Console.Write("\n\n");	
+  }
+}
